a08836391c3a9cb79bf1706bda26aec940987233
Magicpants <arnis@gmail.com>
2025-09-08 11:19:14 +0300

    models

---
+++ app/Models/Quiz.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quiz extends Model
+{
+    use HasFactory;
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
+<<<<<<< HEAD
 use App\Http\Controllers\RegisterController;
 
 Route::get('/register', [RegisterController::class, "create"])->middleware("guest");
@@ -10,7 +11,19 @@ Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/login', [SessionController::class, "create"])->name("login")->middleware("guest");
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
+=======
+use App\Http\Controllers\QuizController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\ResultController;
+
+>>>>>>> a1bfed0 (Modeli done)
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::resource('quizzes', QuizController::class);
+Route::resource('questions', QuestionController::class);
+Route::resource('results', ResultController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::resource('quizzes', QuizController::class);
 });
