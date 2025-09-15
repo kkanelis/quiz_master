c4e68f84317d4b329c22f32d8478edc6a7e1a8ff
Magicpants <arnis@gmail.com>
2025-09-15 10:37:27 +0300

    Try

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
-
+use App\Http\Controllers\ResultController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\QuizController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\RegisterController;
 
 Route::get('/register', [RegisterController::class, "create"])->middleware("guest");
@@ -11,12 +12,6 @@ Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/login', [SessionController::class, "create"])->name("login")->middleware("guest");
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
-=======
-use App\Http\Controllers\QuizController;
-use App\Http\Controllers\QuestionController;
-use App\Http\Controllers\ResultController;
-
->>>>>>> a1bfed0 (Modeli done)
 
 Route::get('/', function () {
     return view('welcome');
